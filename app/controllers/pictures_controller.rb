7707085eefd63b7537e64730f74d391f298d4e3a
Nyron-Waite <nyroncurtiswaite@gmail.com>
2016-04-25 12:21:13 -0400

    updated picture controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,11 @@
 class PicturesController < ApplicationController
 
+  before_action do
+    if params[:id]
+      @picture = Picture.find(params[:id])
+    end
+  end
+
 def index
   @pictures = Picture.all
   respond_to do |format|
@@ -16,6 +22,21 @@ end
 def create
    @picture = Picture.create!(picture_params)
    render json: @picture, status: :ok
+end
+
+def update
+  @picture.update!(picture_params)
+  render json: @picture, status: :ok
+end
+
+def destroy
+  @picture.destroy
+  render json: {success: true}, status: :ok
+end
+
+private
+def picture_params
+  params.require(:picture).permit(:name, :summary, :image, :rank)
 end
 
 end
